836e376da4a5541ee7cfd0d9d36d1487d5b3f9f8
Githmi Niseka <108750891+GithmiNiseka@users.noreply.github.com>
2025-02-06 22:25:45 +0530

    Add files via upload

---
+++ src/img/growMateCaseStudy.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import growMateCasestudyImg from '../img/GrowMateCaseStudy.png';
+
+const growMateCaseStudy = () => {
+  return (
+    <div className='lmsCSDisplay'>
+             
+               <img src={growMateCasestudyImg} className='lmsCaseStudy'  alt='lexTrustCaseStudy'/>
+           </div>
+  )
+}
+
+export default growMateCaseStudy
