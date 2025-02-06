da78fff66efc74834c4aef0b9059ff8362daff1b
Githmi Niseka <108750891+GithmiNiseka@users.noreply.github.com>
2025-02-06 22:25:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Components/Home.js'
 import LMSCaseStudy from './Components/lmsCaseStudy';
 import LexTrustCaseStudy from './Components/lexTrustCaseStudy.js';
+import GrowMateCaseStudy from './Components/growMateCaseStudy.js';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/lmsCaseStudy" element={<LMSCaseStudy />} />
         <Route path="/lexTrustCaseStudy" element={<LexTrustCaseStudy />} />
+        <Route path="/growMateCaseStudy" element={<GrowMateCaseStudy />} />
         </Routes>
       </Router>
       
