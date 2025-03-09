cdf82fbb3a85b1adc50faf09b48b38bc88cef3da
Githmi Niseka <108750891+GithmiNiseka@users.noreply.github.com>
2025-03-10 01:33:33 +0530

    Add files via upload

---
+++ src/Components/mealMapCaseStudy.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import mealMapCasestudyImg from '../img/MealMapCaseStudy.png';
+
+const mealMapCaseStudy = () => {
+  return (
+   <div className='lmsCSDisplay'>
+                
+                  <img src={mealMapCasestudyImg} className='lmsCaseStudy'  alt='lexTrustCaseStudy'/>
+              </div>
+  )
+}
+
+export default mealMapCaseStudy
